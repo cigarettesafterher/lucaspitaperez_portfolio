33272210390bc4fa520bd2d841741778e62ac719
cigarettesafterher <lucaspitaperex.work@gmail.com>
2022-07-06 00:58:09 +0200

    first commit

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,5 @@
-import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
-const Header = () => {
-  ;
+const Header = () => {;
 
   return (
     <div className="hidden md:block container mx-auto px-10 mb-2">
--- pages/index.js
+++ pages/index.js
@@ -2,19 +2,19 @@ import { FeaturedPosts } from '../sections/index';
 import { getPosts } from '../services';
 import Intro from '../components/Intro';
 
-export default function Home({}) {
+export default function Home() {
   return (
     <div className="container mx-auto px-10 mb-8">
 
       <Intro />
       <FeaturedPosts />
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-      {' '}
+        {' '}
 
         <div className="lg:col-span-4 col-span-1">
-        {' '}
-          <div className="lg:sticky relative top-8">
           {' '}
+          <div className="lg:sticky relative top-8">
+            {' '}
           </div>
         </div>
       </div>
